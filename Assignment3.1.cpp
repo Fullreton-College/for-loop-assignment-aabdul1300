36183e72892293bc6000fbe75cd63fcad08decb1
aabdul1300 <abdulmananar2005@gmail.com>
2026-03-01 13:07:22 -0800

    Update Assignment3.1.cpp

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,27 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
-#include <iomanip>
-using namespace std;
+#include <vectors>
+#include <string>
 
-int main()
-{
+int main() {
+
+  std::vectors<int> salesData(5)
+
+for (int i = 0; i < 5; ++i) {
+        std::cout << "Enter today's sales for store " << (i + 1) << ": ";
+        std::cin >> salesData[i];
+    }
+
+    std::cout << "\nSALES BAR CHART" << std::endl;
+    std::cout << "(Each * = $100)" << std::endl;
+
+  
+    for (int i = 0; i < 5; ++i) {
+       
+        int numAsterisks = salesData[i] / 100;
+        std::cout << "Store " << (i + 1) << ": " 
+                  << std::string(numAsterisks, '*') << std::endl;
+    }
 
 return 0;
+
 }
